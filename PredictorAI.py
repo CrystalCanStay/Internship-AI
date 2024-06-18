fb90b51e3078bd11ecabd0b9b3488440a5578f54
Crystal <98057908+CrystalCanStay@users.noreply.github.com>
2024-06-18 10:19:31 -0600

    Start of project

---
+++ PredictorAI.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+import matplotlib
+import Reader
+
+weights = [.2, .8, -.5]
+bias = 3
+output = float()
+
+for i in range(Reader.inputData.__len__()):
+    output += (Reader.inputData[i])*(weights[i])
+output += bias
+print(Reader.inputData.__len__())
+print(output)
---
+++ Reader.py
@@ -0,0 +1,10 @@
+
+
+
+# This is going to be the list we later fill up by reading from a file
+inputData = []
+
+# placeholder for doing basic neuron stuff
+inputData.insert(0, 1)
+inputData.insert(1, 2)
+inputData.insert(2, 3)
